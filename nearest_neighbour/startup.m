a4830601c19bd1dd96bab0892d88116dd674ae58
John Kishore M <johnkishorem@gmail.com>
2017-07-01 20:31:42 +0530

    K nearest neighbour extended to test test

--- nearest_neighbour/startup.m
+++ nearest_neighbour/startup.m
@@ -13,23 +13,35 @@ X_cv = X((train_len +1) : train_len + cv_len, :); y_cv = y((train_len +1) : trai
 X_test = X((cv_len +1) : cv_len + test_len, :); y_test = y((cv_len +1) : cv_len + test_len);
 
 %% Using CV to select a value for K
-error_history = zeros(100,1);
-for iter = 1:100
-    fprintf('Running nearest neighbout with %d neighbours',iter);
+count = 10;
+error_history = zeros(count,1);
+for iter = 1:count
+    fprintf('\nRunning nearest neighbout with %d neighbours',iter);
     k = iter;
     mean_k_nn = zeros(cv_len,1);
     for i = 1:cv_len
-        k_nearest_neighbours = find_nearest_neighbours(X_train, X_test(i,:), k);
+        k_nearest_neighbours = find_nearest_neighbours(X_train, X_cv(i,:), k);
         sum_help = sum(y(k_nearest_neighbours));
         mean_k_nn(i) = sum_help / k;
     end
     squared_error = (y_cv - mean_k_nn) .^ 2;
     mean_error = sum(squared_error) / cv_len;
-    root_mean_square = mean_error ^ 0.5
+    root_mean_square = mean_error ^ 0.5;
     error_history(iter) = root_mean_square;
 end
 plot(error_history,1:numel(error_history));
- 
+
+%% Error on test set
+k = 1;
+for i = 1:test_len
+    k_nearest_neighbours = find_nearest_neighbours(X_train, X_test(i,:), k);
+    sum_help = sum(y(k_nearest_neighbours));
+    mean_k_nn(i) = sum_help / k;
+end
+squared_error = (y_test - mean_k_nn) .^ 2;
+mean_error = sum(squared_error) / test_len;
+root_mean_square = mean_error ^ 0.5
+
 %% Mean of K nearest neighbour
 k_nearest_neighbours = find_nearest_neighbours(X_train, X_test(1,:), k);
 mean_k_nn = sum(y(k_nearest_neighbours));
